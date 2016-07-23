d4f9da520f3b66cc41d53dbede2f8936022a82b3
kiran paturi <kpaturi@smarttuition.com>
2016-07-23 10:36:55 +0530

    added code

--- app/components/components.js
+++ app/components/components.js
@@ -1,1 +1,29 @@
 angular.module("components", []);
+angular.module("components")
+    .directive("customDatePicker", [function () {
+        return {
+            restrict: "A",
+            link: function (scope, element, attrs) {
+                var config = {};
+                if (attrs["max"]) {
+                    config.maxDate = attrs["max"];
+                }
+                if (attrs["min"]) {
+                    config.minDate = attrs["min"];
+                }
+
+                element.datepicker(config);
+            }
+        }
+}]);
+
+angular.module("components")
+    .directive("customSlider", [function () {
+        return {
+            restrict: "A",
+            templateUrl: "app/components/customSlider.html",
+            link: function (scope, element, attrs) {
+                $(element).find(".bxslider").bxSlider();
+            }
+        }
+            }]);
